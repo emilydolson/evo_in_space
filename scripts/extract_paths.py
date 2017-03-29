77ad3caac497671688c3dc637e5583de2536ad1a
Emily Dolson <EmilyLDolson@gmail.com>
2017-03-29 03:47:15 -0400

    added path extraction script

---
+++ scripts/extract_paths.py
@@ -0,0 +1,18 @@
+import glob
+import sys
+
+num = sys.argv[1]
+
+filenames = glob.glob("*/lineage_locs_"+num+".dat")
+
+outfile = open("paths_"+num+".dat")
+
+for name in filenames:
+    infile = open(name)
+    path = infile.readline().split()[1:-1]
+    infile.close()
+    path = [int(i) for i in path]
+    path = [[i%60, i//60] for i in path]
+    outfile.write(path)
+
+outfile.close()
