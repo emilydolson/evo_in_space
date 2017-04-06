7a83df1891256f59366f90c3a34112e4a9a776a7
Emily Louise Dolson <emilyldolson@gmail.com>
2017-04-06 16:56:02 -0400

    Collect control points

--- scripts/collect_points.py
+++ scripts/collect_points.py
@@ -5,8 +5,12 @@ outfile = open("all_task_locs.csv", "w")
 outfile.write("task,x,y,update,rep,environment\n")
 files = glob.glob("*/task_locs.csv")
 for filename in files:
-    env = filename.split("/")[-2].split("_")[1]
-    rep = filename.split("/")[-2].split("_")[-1]
+    split_name = filename.split("/")[-2].split("_")
+    if split_name[0] == "control":
+        env = "control"
+    else:
+        env = split_name[1]
+    rep = split_name[-1]
     infile = open(filename)
     for line in infile.readlines():
         outfile.write(line.strip() + "," + rep + "," + env + "\n")
