bb61725e97ad731a28e68810204ea13aadc33bf7
Emily Dolson <EmilyLDolson@gmail.com>
2017-04-13 15:00:54 -0400

    Added extract_grids.py

---
+++ scripts/extract_grids.py
@@ -0,0 +1,14 @@
+import shutil
+import sys
+
+updates = []
+loc_file = open("task_locs.csv")
+for line in loc_file:
+    if line.strip() == "":
+        continue
+    updates.append(int(line.split(",")[3]))
+loc_file.close()
+updates.sort()
+
+for update in updates:
+    shutil.copy("data/grid_task."+str(update)+".dat*", sys.argv[1])
